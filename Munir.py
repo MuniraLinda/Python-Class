220411bd3a1c0be0eef6d3db39622c5aac5a9182
MuniraLinda <82053808+MuniraLinda@users.noreply.github.com>
2021-06-11 12:53:09 +0000

    Create Munir.py
    Second lines

---
+++ Munir.py
@@ -0,0 +1,20 @@
+#First Assignment : print statement
+print("My name is Muniratu Linda Fianyeku")
+
+#Second Assignment : Declare a variable using
+'''
+Name        
+Age 
+Marital status
+Country  
+'''
+
+Name = "Muniratu Linda Fianyeku"
+Age = 24
+Marital_status = "Single"
+Country = "Ghana"
+
+print(Name)
+print(Age)
+print(Marital_status)
+print(Country)
