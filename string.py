998419b83b376929be626247e0700c5814d7314d
MuniraLinda <82053808+MuniraLinda@users.noreply.github.com>
2021-06-12 18:39:05 +0000

    Create string.py

---
+++ string.py
@@ -0,0 +1,22 @@
+#Assignment : String Literals
+# 1. using string method, convert your lower case name to upper case
+
+name = "Muniratu Linda Fianyeku"
+print(name.upper())
+
+#2. Using string method replace "N" in Nana kwame with "B"
+Instructor = "Nana kwame" 
+print(Instructor.replace("N","B"))
+
+#3. write a program of two variables age and gender and using format method and insert into a print statement of "My {age} is and I am a {gender}"
+age = 24
+gender = "female"
+print("My age is {} and I am a {}.".format(age,gender))
+
+#4. using the f method,print statement of "My {age} is and I am a {gender}.
+print(f"My age is {age} and I am a {gender}.")
+
+#Get the chraracter position for 2 in "Hi Ladies"
+
+a = "Hi Ladies"
+print(a[2])
