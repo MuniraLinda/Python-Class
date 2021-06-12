42543c1053e3a687fda4e0ab27467f617107c0c1
MuniraLinda <82053808+MuniraLinda@users.noreply.github.com>
2021-06-12 16:25:15 +0000

    Create numbers.py
    numbers assignment

---
+++ numbers.py
@@ -0,0 +1,13 @@
+#Asignment : numeric datatypes
+'''
+1. Using the random library,
+Print out random numbers range from 10-100
+2. Convert float to int
+'''
+
+import random
+
+print(random.randrange(10,100))
+
+#x = 35.3
+#print(int(x))
