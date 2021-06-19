27ef3d115691c5433aa3262543e640d150b41df0
MuniraLinda <82053808+MuniraLinda@users.noreply.github.com>
2021-06-20 00:01:38 +0100

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,24 @@
+#Assignement : boolean
+
+#1. Using an if statement to check a boolean value compare 100 1nd 1000
+a = 100
+b = 1000
+if a < b:
+    print("Yes 100 is less than 1000")
+
+
+#2.Using an if statement to check a boolean value compare 10 1nd 100
+x = 10
+y = 100
+if x < y:
+    print("Yes 10 is less than 100")
+
+
+# Evaluate 23 > 34, 45 > 11, 98 < 23, 12 == 13
+print(23 > 34)
+
+print(45 > 11)
+
+print(98 < 23)
+
+print(12 == 13)
