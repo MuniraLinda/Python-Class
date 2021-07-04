ebb0d7d583d6013c2d39fe50b91c6afcc1f263a5
MuniraLinda <82053808+MuniraLinda@users.noreply.github.com>
2021-07-04 09:24:49 +0000

    commit

--- function.py
+++ function.py
@@ -18,3 +18,37 @@ first_function()
 second_function()
 agesum()
 inp()
+
+
+def Add(a,b):
+    c = a + b
+    return c
+
+a = Add(12,56)
+print(a)
+
+def Add(a,b,d):
+    c = a + b - d
+    return c
+
+a = Add(12,56,45)
+print(a)
+
+def arbarg(*kids):
+    print("The oldest child is " + kids[2])
+
+arbarg('nana','kojo','keame')
+
+
+def my_function(**kid):
+    print("his last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = 'refsnes')
+
+def cout(Country = "Ghana"):
+    print("My country is " + Country)
+
+cout()
+
+def for_loop(food):
+
